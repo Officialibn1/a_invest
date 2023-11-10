67d7d8b9a7b2c5b4b0f601a03bb71cba56444553
ibn <officialibn001@gmail.com>
2023-11-10 14:07:37 +0100

    added profile image

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
 	return (
-		<div>
+		<div className='py-10 flex flex-col items-center gap-12'>
 			<h1>Home Page</h1>
+
+			<Link
+				className='bg-indigo-600 text-xl px-3 py-2 rounded-xl hover:bg-indigo-500 duration-200 ease-in-out text-slate-200'
+				href={"/dashboard"}>
+				Dashboard
+			</Link>
 		</div>
 	);
 };
